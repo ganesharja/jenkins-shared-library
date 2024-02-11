b3024c2974209cfa35896e1b1dd46844c351d92c
ganesh <jayaganesharja@gmail.com>
2024-02-12 00:16:14 +0530

    auto push

---
+++ vars/nodejs.groovy
@@ -0,0 +1,23 @@
+def call () {
+    pipeline {
+        agent any 
+        stages {
+            stage('Lint Checks') {
+                steps {
+                    sh "echo Installing JSlist"
+                    sh "npm i jslint"
+                    sh "echo starting lintchecks"
+                    sh "node_modules/jslint/bin/jslint.js server.js || true"
+                    sh "echo lintChecks completed"
+                }
+            }
+            stage('Generating Artifacts') {
+                steps {
+                    sh "echo Generating Artifacts..."
+                    sh "npm install" 
+                }
+            }
+        }
+    } 
+
+}
