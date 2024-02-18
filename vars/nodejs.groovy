a90045ab813e6764f7a12ef07da1cfb65a6302da
ganesh <jayaganesharja@gmail.com>
2024-02-18 23:27:19 +0530

    auto push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -12,10 +12,6 @@ def call () {
     
     pipeline {
         agent any
-        environment {
-            SONAR_URL = "172.31.9.210"
-            SONAR_CRED  = credentials('SONAR_CRED')
-        }
         stages {
             stage('Lint Checks') {
                 steps {
@@ -26,8 +22,8 @@ def call () {
             }
             stage('Sonar Checks') {
                 steps {
-                    sh "Excuting sonarchecks"
-                    sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=cart -Dsonar.login=admin -Dsonar.password=password"
+                    sh "env"
+                    sh "sonar-scanner -Dsonar.host.url=http://172.31.9.210:9000/ -Dsonar.sources=. -Dsonar.projectKey=cart -Dsonar.login=admin -Dsonar.password=password"
                     // sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gate.sh"
                 }
             }
